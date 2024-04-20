b1f0def134f308a5b9c58bb6f0a65604a9fd77df
Al Ifran <ifran15-2915@diu.edu.bd>
2024-04-20 16:10:19 +0600

    navbar icon added

--- src/components/SideNav/SideNavbar.jsx
+++ src/components/SideNav/SideNavbar.jsx
@@ -10,41 +10,88 @@ const SideNavbar = () => {
                     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-fit md:w-80 min-h-full bg-base-200 text-base-content">
                         {/* Sidebar content here */}
+
                         <li>
-                            <Link>Dashboard</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/Dashboard black.png" alt="Dashboard icon"/>
+                            Dashboard
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Branches</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/Branch black.png" alt="branch icon"/>
+                            Branches
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Reserve Fuel</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/Reserve Fuel black.png" alt="Dashboard icon"/>
+                            Reserve Fuel
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Employee</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/Employee black.png" alt="Dashboard icon"/>
+                            Employee</Link>
                         </li>
+
                         <li>
-                            <Link>Fuel Status</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/Fuel Status black.png" alt="Dashboard icon"/>
+                            Fuel Status
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>User Maintanance</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/user maintanance black.png" alt="Dashboard icon"/>
+                            User Maintanance
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>HRM</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/hrm black.png" alt="Dashboard icon"/>
+                            HRM
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Asset Section</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/asset section black.png" alt="Dashboard icon"/>
+                            Asset Section
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Liabilities</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/liabilities black.png" alt="Dashboard icon"/>
+                            Liabilities
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Financial status</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/financial status black.png" alt="Dashboard icon"/>
+                            Financial status
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Settings</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/settings black.png" alt="Dashboard icon"/>
+                            Settings
+                            </Link>
                         </li>
+
                         <li>
-                            <Link>Logout</Link>
+                            <Link>
+                            <img className="w-6 h-6" src="/public/Side Navbar Icon/logout black.png" alt="Dashboard icon"/>
+                            Logout
+                            </Link>
                         </li>
 
                     </ul>
