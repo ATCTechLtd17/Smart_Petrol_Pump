fd74f10c4c723cd061dd032f05529a856059e2a4
Nashitgit <nazmulnashitofficial@gmail.com>
2024-05-11 20:32:00 +0600

    first section

--- src/page/Branches/AddNewBranches.jsx
+++ src/page/Branches/AddNewBranches.jsx
@@ -25,13 +25,31 @@ const AddNewBranches = () => {
       </section>
 
 
-      <div className="m-4">
-        <div className=" ">
-          <Label name={"branchName"}/>
+      <div className="m-4 grid grid-cols-2 gap-4">
+
+        <div className="flex flex-col gap-1">
+          <Label name={"branchName"} />
           <TextInput onChange={handleChange} name={"branchName"} placeHolder={"enter your name"} />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Label name={"branchCode"} />
+          <NumberInput onChange={handleChange} name={"branchCode"} placeHolder={"enter code"} />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Label name={"address"} />
+          <TextInput onChange={handleChange} name={"address"} placeHolder={"enter your address"} />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Label name={"status"} />
 
         </div>
-        <div>
+        <div className="">
+
+          <Label name={"hostBranch"} />
+
+        </div>
+
+        {/* <div>
           <NumberInput />
 
         </div>
@@ -40,7 +58,11 @@ const AddNewBranches = () => {
         <form action="">
           <GreenButton name="Submit" onClick={handleSubmit} />
           <NumberInput />
-        </form>
+        </form> */}
+      </div>
+
+      <div className="w-full flex justify-center mt-8">
+        <GreenButton name={"Submit"} />
       </div>
 
     </div>
