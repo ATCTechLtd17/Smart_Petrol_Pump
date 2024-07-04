55892f5c96bfb56293e4f08b72a7a1d505352983
joyhasan08 <rubayedhasan235@gmail.com>
2024-07-05 02:55:16 +0600

    dashbord update

--- src/page/Branches/BranchesNav/BranchesNav.jsx
+++ src/page/Branches/BranchesNav/BranchesNav.jsx
@@ -4,10 +4,10 @@ const BranchesNav = () => {
     <div>
 
       <nav className="bg-[#0F79B9] text-white ">
-        <div className="w-fit mx-auto px-2 sm:px-6 lg:px-8">
-          <div className="relative w-full flex items-center justify-between h-16">
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
+        <div className="w-full px-2 sm:px-6 ">
+          <div className="relative flex items-center justify-between h-16 w-full ">
+            <div className="hidden sm:block sm:ml-6 w-full ">
+              <div className="flex justify-start  w-full rounded-md space-x-4">
 
                 <Link to={'/add_new_branch'}>
                   <p className=" hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Add New Branch</p>
